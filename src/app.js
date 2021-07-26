29eeff1933c9ee29886a2acc4cc7f277e31151d9
DuanHongFei <1715106673@qq.com>
2021-07-26 17:05:25 +0800

    feat:添加error页和404页

--- src/app.js
+++ src/app.js
@@ -9,12 +9,20 @@ const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
 
 const { REDIS_CONF } =  require('./conf/db')
+const { isProd } = require('./utils/env')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
-// error handler
-onerror(app)
+// error handlerc
+let onerrorConf =  {}
+if (isProd) {
+  onerrorConf = {
+    redirect: '/error'
+  }
+}
+onerror(app, onerrorConf)
 
 // middlewares
 app.use(bodyparser({
@@ -43,17 +51,10 @@ app.use(session({
   })
 }))
 
-// logger
-// app.use(async (ctx, next) => {
-//   const start = new Date()
-//   await next()
-//   const ms = new Date() - start
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-// })
-
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
