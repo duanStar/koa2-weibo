21b463e5ff5ec3be9d417295881ea82dd47c4078
DuanHongFei <1715106673@qq.com>
2021-07-28 17:41:57 +0800

    fix: 判断已登录下的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,14 +5,32 @@
 
 const router = require('koa-router')()
 
+/**
+ * 获取登录信息
+ * @param {Object} ctx koa ctx 
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false,
+  }
+  const userInfo = ctx.session.userInfo
+  if(userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName,
+    }
+  }
+  return data
+}
+
 // login
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 // register
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
